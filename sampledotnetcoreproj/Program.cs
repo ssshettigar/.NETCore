b439e6e000765efd8f01dad19469f1d368f6996d
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2020-11-18 21:06:19 -0500

    Console .NET core App

---
+++ sampledotnetcoreproj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sampledotnetcoreproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
